39449ffc00338af6e95fcc1bbf1f659f246e6163
cisnux <fajrarisqulla@gmail.com>
2025-06-10 21:26:06 +0700

    chores: update dto

--- src/main/java/com/bni/bni/controller/AuthController.java
+++ src/main/java/com/bni/bni/controller/AuthController.java
@@ -44,7 +44,7 @@ public class AuthController {
         if (token != null) {
             response.put("status", 200);
             response.put("token", token);
-            response.put("message", "Successfully logged in 😈😎triasl wkwkwkwk");
+            response.put("message", "Successfully logged in 😈😎triaslsss wkwkwkwk");
             return ResponseEntity.ok(response);
         } else {
             response.put("status", 401);
